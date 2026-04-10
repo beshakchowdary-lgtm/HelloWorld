1cd12a6becc0cb74ae1770585347b0f1882ea5dc
beshakchowdary-lgtm <beshakchowdary@gmail.com>
2026-04-10 13:47:20 +0530

    UC5: Enhanced for loop for multiple names with default World

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,11 @@
-public class HelloApp {
+public class Main {
     public static void main(String[] args) {
-        // args[0] is the first word typed after the command
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello World");
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
     }
 }
