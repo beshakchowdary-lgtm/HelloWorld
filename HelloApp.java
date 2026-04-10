ee7fe9b9784a535e2476da291c16f94961793ebf
beshakchowdary-lgtm <beshakchowdary@gmail.com>
2026-04-10 13:00:31 +0530

    Refactor HelloApp to Main with simplified output
    Removed conditional greeting and simplified main method.

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,5 @@
-public class HelloApp {
+public class Main {
     public static void main(String[] args) {
-        // args[0] is the first word typed after the command
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
-        }
+        System.out.println("Hello " + args[0]);
     }
 }
